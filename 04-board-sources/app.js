09d8472aea80865af61bfe3549cf838274318976
oalbukova <o.albukova@gmail.com>
2021-12-20 00:41:29 +0300

    colors

--- 04-board-sources/app.js
+++ 04-board-sources/app.js
@@ -1,6 +1,6 @@
 const  board = document.querySelector('#board');
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
-const SQUARES_NUMBER = 500;
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71', '#008080', '#F08080', '#DB7093', '#FF6347', '#F0E68C', '#FFDAB9', '#6A5ACD', '#BC8F8F', '#DA70D6', '#BC8F8F', '#5F9EA0', '#6495ED'];
+const SQUARES_NUMBER = 1500;
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div');
