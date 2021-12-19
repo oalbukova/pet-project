6f7aca9b7fc9ade898f3030156f76c82e8226e82
oalbukova <o.albukova@gmail.com>
2021-12-19 23:54:05 +0300

    key

--- 03-slider-sources/app.js
+++ 03-slider-sources/app.js
@@ -6,6 +6,14 @@ const container = document.querySelector('.container');
 const slidesCount = mainSlide.querySelectorAll('div').length;
 let activeSlideIndex = 0;
 
+document.addEventListener('keydown', event => {
+  if (event.key === 'ArrowUp') {
+    changeSlide('up');
+  } else if (event.key === 'ArrowDown') {
+    changeSlide('down');
+  }
+})
+
 sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
 
 upBtn.addEventListener('click', () => {
